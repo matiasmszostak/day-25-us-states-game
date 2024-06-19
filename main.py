f5bbc97d39d1a68c78bbffbb566b09e5182e0896
Matías Szostak <mszostak@asjservicios.com>
2024-06-19 13:12:42 -0300

    Added get_state_location() and missing_states_csv(), to get the location and create a csv file with the missing states for the player to learn. Added an exit for the game.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from turtle import Turtle, Screen
 import pandas
 
-
 screen = Screen()
 screen.title("U.S. State Game")
 IMAGE = "blank_states_img.gif"
@@ -19,37 +18,44 @@ t.shape(IMAGE)
 #
 # turtle.mainloop()
 
-#Check if the guess is among the 50 states
+# Check if the guess is among the 50 states
 # Guardar los estados en una lista
 # Recorrer la lista y ver si la respuesta coincide
 # Si coincide, devolver True (por ahora)
 
 data = pandas.read_csv("50_states.csv")
 
-
 states_list = data["state"].to_list()
 
 correct_answers = 0
 
-
-
-
-
 FONT = ("Arial", 8, "bold")
 
-def write_state(position, state):
+
+def write_state(position, state, color):
     turtle = Turtle()
+    turtle.speed("fastest")
     turtle.hideturtle()
     turtle.penup()
     turtle.goto(position)
-    turtle.pencolor("green")
+    turtle.pencolor(color)
     turtle.write(state, align="center", font=FONT)
 
 
+def get_state_location(state_param):
+    state_data = data[data.state == state_param]
+    return int(state_data.x.iloc[0]), int(state_data.y.iloc[0])
+
+
+def missing_states_csv():
+    missing_states_dict = {
+        "Missing States": states_list,
+    }
+    df_missing_states = pandas.DataFrame(missing_states_dict)
+    df_missing_states.to_csv("states_to_learn.csv")
 
 
 title = ""
-
 
 game_is_on = True
 
@@ -63,24 +69,25 @@ while game_is_on:
     answer_state = screen.textinput(title=title, prompt="What's the State's name?").title()
     print(answer_state)
 
-    if answer_state in states_list:
+    if answer_state == "Exit":
+        for state in states_list:
+            location_position = get_state_location(state)
+            write_state(location_position, state, color="red")
+
+            # Create a csv file with all the missing States so the player can learn them better.
+            missing_states_csv()
+        break
+
+    elif answer_state in states_list:
         states_list.remove(answer_state)
         correct_answers += 1
 
+        location_position = get_state_location(answer_state)
 
-        state_data = data[data.state == answer_state]
-        location_position = (int(state_data.x.iloc[0]), int(state_data.y.iloc[0]))
+        write_state(location_position, answer_state, color="green")
 
-
-        write_state(location_position, answer_state)
-
-        #TODO: Test this code
         if len(states_list) == 0:
+            missing_states_csv()
             game_is_on = False
-
-
-
-
-
 
 screen.exitonclick()
